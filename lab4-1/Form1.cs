78391e257f4b63cfbf33f9fa0769192af5c0df13
Dinara L <dinaralatypova16@mail.ru>
2025-02-18 13:29:07 +0500

    lab4 added

---
+++ lab4-1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace lab4_1
+{
+    public partial class Form1 : Form
+    {
+        private List<CCircle> circles = new List<CCircle>();
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+
+            this.MouseDown += Form1_MouseDown;
+            this.Paint += Form1_Paint;
+            this.KeyDown += Form1_KeyDown;
+            this.Resize += Form1_Resize;
+            this.KeyPreview = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int countCircle = 0;
+                foreach (var circle in circles)
+                {
+                    if (circle.Contains(e.Location))
+                    {
+                        countCircle += 1;
+                        if (Control.ModifierKeys != Keys.Control)
+                        {
+                            if (countCircle == 1 || !checkBox1.Checked) 
+                                foreach (var c in circles) c.Deselect();
+                            circle.SetSelection();
+                        }
+                        else
+                        {
+                            circle.SetSelection();
+                        }
+                    }
+                }
+
+                if (countCircle == 0)
+                {
+                    foreach (var c in circles) c.Deselect();
+                    CCircle circle = new CCircle(e.Location);
+                    circles.Add(circle);
+                }
+
+                this.Invalidate();
+            }
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var circle in circles)
+            {
+                circle.Draw(e.Graphics);
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                circles.RemoveAll(c => c.IsSelected);
+                this.Invalidate();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            circles.Clear();
+            this.Invalidate();
+        }
+    }
+
+    public class CCircle
+    {
+        private const int radius = 30;
+        private Point center;
+        private bool isSelected;
+
+        public CCircle(Point center)
+        {
+            this.center = center;
+            this.isSelected = false;
+        }
+
+        public CCircle()
+        {
+            
+        }
+
+        public Point Center => center;
+        public bool IsSelected => isSelected;
+
+        public void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(Color.Red))
+            {
+                g.FillEllipse(brush, center.X - radius, center.Y - radius, radius * 2, radius * 2);
+            }
+            if (isSelected)
+            {
+                using (Brush brush = new SolidBrush(Color.Blue)) { g.FillEllipse(brush, center.X - radius, center.Y - radius, radius * 2, radius * 2); }
+                using (Brush brush = new SolidBrush(Color.Red)) { g.FillEllipse(brush, center.X - radius + 2, center.Y - radius + 2, radius * 2 - 4, radius * 2 - 4); }
+            }
+        }
+
+        public bool Contains(Point point)
+        {
+            return (point.X - center.X) * (point.X - center.X) + (point.Y - center.Y) * (point.Y - center.Y) <= radius * radius;
+        }
+
+        public void SetSelection()
+        {
+            isSelected = true;
+        }
+
+        public void Deselect()
+        {
+            isSelected = false;
+        }
+    }
+
+}
